ed10ee4c7783c5495d137d574e71e93febf8029f
rashikagupta <rgu245@uky.edu>
2022-06-15 14:51:03 -0400

    Initial commit

---
+++ foilBothSide-DeadLayerLogVol-3Micron/build/CMakeFiles/NaB.dir/DependInfo.cmake
@@ -0,0 +1,56 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/newNab.cc" "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/build/CMakeFiles/NaB.dir/newNab.cc.o"
+  "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/src/UCNBAnalysisManager.cc" "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/build/CMakeFiles/NaB.dir/src/UCNBAnalysisManager.cc.o"
+  "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/src/UCNBDetectorConstruction.cc" "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/build/CMakeFiles/NaB.dir/src/UCNBDetectorConstruction.cc.o"
+  "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/src/UCNBDetectorMessenger.cc" "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/build/CMakeFiles/NaB.dir/src/UCNBDetectorMessenger.cc.o"
+  "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/src/UCNBEventAction.cc" "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/build/CMakeFiles/NaB.dir/src/UCNBEventAction.cc.o"
+  "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/src/UCNBField.cc" "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/build/CMakeFiles/NaB.dir/src/UCNBField.cc.o"
+  "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/src/UCNBPhysicsList.cc" "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/build/CMakeFiles/NaB.dir/src/UCNBPhysicsList.cc.o"
+  "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/src/UCNBPrimaryGeneratorAction.cc" "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/build/CMakeFiles/NaB.dir/src/UCNBPrimaryGeneratorAction.cc.o"
+  "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/src/UCNBRunAction.cc" "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/build/CMakeFiles/NaB.dir/src/UCNBRunAction.cc.o"
+  "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/src/UCNBStackingAction.cc" "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/build/CMakeFiles/NaB.dir/src/UCNBStackingAction.cc.o"
+  "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/src/UCNBSteppingAction.cc" "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/build/CMakeFiles/NaB.dir/src/UCNBSteppingAction.cc.o"
+  "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/src/UCNBSteppingVerbose.cc" "/mnt/gpfs2_4m/scratch/rgu245/Now/0622/deadlLayerLogicalVolremove1MicronThickness-june9/build/CMakeFiles/NaB.dir/src/UCNBSteppingVerbose.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4UI_USE_XM"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_OPENGLXM"
+  "G4VIS_USE_RAYTRACERX"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/scif/apps/clhep/include"
+  "/scif/apps/geant4/include/Geant4"
+  "/usr/include/QtCore"
+  "/usr/include/QtGui"
+  "/usr/include/QtOpenGL"
+  "../include"
+  "/scif/apps/root/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
